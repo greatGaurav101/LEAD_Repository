cb7b2bcf6507ba3b0c0af80871ab7a6571a01dab
amit ranjan <amitranjan876@gmail.com>
2021-04-14 15:36:56 +0530

    first commit .....code to reverse a given string !!

---
+++ practice_008/src/practice_008/tc_001.java
@@ -0,0 +1,15 @@
+package practice_008;
+
+public class tc_001 {
+
+	public static void main(String[] args) {
+		
+		 String str="mahabharat";
+	     
+	     for(int i=str.length()-1; i>=0; i--){
+	       System.out.print(str.charAt(i));
+
+	     } 
+	}
+
+}
