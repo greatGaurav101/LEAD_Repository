f24e2412fae2cbed5ab123b206f2cded7d0cfc86
amit ranjan <amitranjan876@gmail.com>
2021-05-13 17:05:21 +0530

    change in String of tc001

--- practice_008/src/practice_008/tc_001.java
+++ practice_008/src/practice_008/tc_001.java
@@ -4,7 +4,7 @@ public class tc_001 {
 
 	public static void main(String[] args) {
 		
-		 String str="mahabharat";
+		 String str="mahabharata is an epic ";
 	     
 	     for(int i=str.length()-1; i>=0; i--){
 	       System.out.print(str.charAt(i));
